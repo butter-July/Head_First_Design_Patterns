0c9a4c5baa7edf6887ec1ef3ab79e932f6be31c3
z <1372234588@QQ.COM>
2025-07-12 17:50:24 +0800

    Update remoteControl.go

--- Command/remoteControl.go
+++ Command/remoteControl.go
@@ -3,24 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	remote := SimpleRemoteControl{}
+
+	remote := RemoteControl{}
 	light := Light{}
 	LigthOn := LightOnCommand{&light}
 	Lightoff := LightOffCommand{&light}
-	remote.SetCommand(1, &LigthOn, &Lightoff) //这里不对应该显示on和off
+
+	remote.SetCommand(1, &LigthOn, &Lightoff)
 	remote.OnButtonWasPressed(1)
+	remote.OffButtonWasPressed(1)
+	remote.UndoButtonWasPressed()
 	fmt.Println(remote.getClassName(light))
+
 	ceilingFan := CeilingFan{}
 	ceilingfanon := CeilingFanOn{&ceilingFan}
 	ceilingFanoff := CeilingFanOff{&ceilingFan}
 	remote.SetCommand(2, &ceilingfanon, &ceilingFanoff)
 	remote.OnButtonWasPressed(2)
+	remote.OffButtonWasPressed(2)
+	remote.UndoButtonWasPressed()
 	fmt.Println(remote.getClassName(ceilingFan))
+
 	stere := Stere{}
 	stereon := StereOnOnCommand{&stere}
-	stereoff := StereOffCommand{&stere}    //其他的没进去 and  同上
+	stereoff := StereOffCommand{&stere}
 	remote.SetCommand(3, &stereon, &stereoff)
 	remote.OnButtonWasPressed(3)
-	
+	remote.OffButtonWasPressed(3)
+	fmt.Println(remote.getClassName(3))
+	remote.UndoButtonWasPressed()
 
 }
