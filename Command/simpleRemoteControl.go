032fa2c3d53f72f29f96c09332cb8a1c66000d10
z <1372234588@QQ.COM>
2025-07-12 23:23:34 +0800

    Update simpleRemoteControl.go

--- Command/simpleRemoteControl.go
+++ Command/simpleRemoteControl.go
@@ -6,30 +6,45 @@ import (
 )
 
 type RemoteControl struct {
+
 	//slot Command
-	onCommand   [7]Command
-	offCommand  [7]Command
-	undoCommand Command
+	onCommand      [7]Command
+	offCommand     [7]Command
+	undoCommand    Command
+	commandHistory []Command
 }
 
 func (s *RemoteControl) SetCommand(slot int, oncommand Command, offCommand Command) {
+
 	s.onCommand[slot] = oncommand
 	s.offCommand[slot] = offCommand
+
 }
+
 func (s *RemoteControl) OnButtonWasPressed(slot int) {
 	s.onCommand[slot].execute()
 	s.undoCommand = s.onCommand[slot]
+	s.commandHistory = append(s.commandHistory, s.onCommand[slot])
 }
 
 func (s *RemoteControl) OffButtonWasPressed(slot int) {
 	s.offCommand[slot].execute()
 	s.undoCommand = s.offCommand[slot] //记录现在是什么,但似乎
+	s.commandHistory = append(s.commandHistory, s.offCommand[slot])
 }
+
 func (s *RemoteControl) UndoButtonWasPressed() {
 	if s.undoCommand != nil {
 		s.undoCommand.undo()
 	}
 }
+func (s *RemoteControl) UndoAll() {
+	for i := len(s.commandHistory) - 1; i >= 0; i-- {
+		s.commandHistory[i].undo()
+	}
+	s.commandHistory = []Command{}
+}
+
 func (s *RemoteControl) ToString() {
 	r := fmt.Sprintf("\n-----Remote Control-----\n")
 	for i := range s.onCommand {
@@ -41,6 +56,7 @@ func (s *RemoteControl) ToString() {
 		r += fmt.Sprintf("[slot %d]%s   %s", i, onClass, offClass)
 	}
 }
+
 func (s *RemoteControl) getClassName(myVar interface{}) string { //这段代码的核心是通过反射动态获取变量的类型名称，尤其适用于需要处理多种类型或指针类型的场景。
 	if t := reflect.TypeOf(myVar); t.Kind() == reflect.Ptr {
 		return t.Elem().Name() //是指针那么返回的是他指向了谁的类型名称(自己定义的名字)
