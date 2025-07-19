946b0e6609c129a2f11bccf4dd1fafa7be4920a3
z <1372234588@QQ.COM>
2025-07-19 15:45:57 +0800

    Update hasQuarterState.go

--- State/hasQuarterState.go
+++ State/hasQuarterState.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type HasQuarterState struct {
 	machine *GumballMachine
@@ -13,15 +17,23 @@ func (hasQuarter *HasQuarterState) insertQuarter() {
 func (hasQuarter *HasQuarterState) ejectQuarter() {
 	fmt.Println("Quarter returned")
 	hasQuarter.machine.setState(&hasQuarter.machine.NoQuarterState)
+
 }
 
 func (hasQuarter *HasQuarterState) turnCrank() {
+	rand.Float64()
 	fmt.Println("you can turn,candy is coming....")
-	hasQuarter.machine.SoldState.dispense() ////////////important!!!!!!!!这里要换到SoldState
-	hasQuarter.machine.setState(&hasQuarter.machine.NoQuarterState)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	number := r.Intn(10)
+	if number == 0 {
+		hasQuarter.machine.SoldState.dispense() ////////////important!!!!!!!!这里要换到SoldState
+		hasQuarter.machine.setState(&hasQuarter.machine.WinnerState)
+	} else {
+		hasQuarter.machine.SoldState.dispense() ////////////important!!!!!!!!这里要换到SoldState
+		hasQuarter.machine.setState(&hasQuarter.machine.NoQuarterState)
 
+	}
 	//投币的情况下转动手柄,然后把状态设置为没有币了,然后进入出糖果的行为
-
 }
 
 func (hasQuarter *HasQuarterState) dispense() {
