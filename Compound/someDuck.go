f0544b5ed68140d647a2c7aa358d4a692383a1cd
z <1372234588@QQ.COM>
2025-07-25 16:43:43 +0800

    Update someDuck.go

--- Compound/someDuck.go
+++ Compound/someDuck.go
@@ -7,7 +7,6 @@ type MarllardDuck struct {
 
 func (m *MarllardDuck) quack() {
 	fmt.Println("Quack")
-
 }
 
 type RedheadDuck struct {
@@ -29,4 +28,32 @@ type RubberDuck struct {
 
 func (rr *RubberDuck) quack() {
 	fmt.Println("Squeak")
+}
+
+type AbstractDuckFactory interface {
+	createMallardDuck() Quackable
+	createRedheadDuck() Quackable
+	createDuckCall() Quackable
+	createRubberDuck() Quackable
+	Counter() int
+} //一个工厂来制造鸭子,但是感觉乱七八糟 ,奇奇怪怪的一大堆
+type DuckFactory struct {
+}
+
+func (d *DuckFactory) createMallardDuck() Quackable {
+	return &MarllardDuck{}
+}
+
+func (d *DuckFactory) createRedheadDuck() Quackable {
+	return &RedheadDuck{}
+}
+
+func (d *DuckFactory) createDuckCall() Quackable {
+	return &DuckCall{}
+}
+func (d *DuckFactory) createRubberDuck() Quackable {
+	return &RubberDuck{}
+}
+func (d *DuckFactory) Counter() int {
+	return 0
 }
